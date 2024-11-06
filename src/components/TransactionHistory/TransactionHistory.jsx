300fa7f07c672f6feb4631c13a344c2df56e3c2d
Kudrytska <tdolinkina@gmail.com>
2024-11-06 18:14:11 +0100

    fix css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,23 +2,23 @@ import css from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
     return (
-        <table>
+        <table className={css.table}>
             <thead>
                 <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
+                    <th className={css.itemsHead}>Type</th>
+                    <th className={css.itemsHead}>Amount</th>
+                    <th className={css.itemsHead}>Currency</th>
                 </tr>
             </thead>
         
-            <tbody>
+            <tbody className={css.tableBody}>
                 {items.map((item) => {
                     const { id, type, amount, currency } = item;
                     return (
-                        <tr key={id}>
-                            <td>{type}</td>
-                            <td>{amount}</td>
-                            <td>{currency}</td>
+                        <tr key={id} className={css.lines}>
+                            <td className={css.itemsBody}>{type}</td>
+                            <td className={css.itemsBody}>{amount}</td>
+                            <td className={css.itemsBody}>{currency}</td>
                         </tr>)
                 })}
             </tbody>
